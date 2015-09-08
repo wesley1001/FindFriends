6e877a4e9b65252a2cc7d0c1bf6f16d4ba30d9de
ashkin0529 <617624763@qq.com>
2015-09-08 16:32:04 +0800

    添加 LogUtil 工具类

---
+++ app/src/main/java/com/ashkin/findfriends/util/LogUtil.java
@@ -0,0 +1,75 @@
+package com.ashkin.findfriends.util;
+
+import android.util.Log;
+
+/**
+ * Log util
+ */
+public class LogUtil {
+
+    // Log level
+    public static final int VERBOSE = 1;
+    public static final int DEBUG = 2;
+    public static final int INFO = 3;
+    public static final int WARN = 4;
+    public static final int ERROR = 5;
+    public static final int NOTHING = 6;
+
+    // current level
+    public static final int LEVEL = VERBOSE;
+
+    /**
+     * Log Verbose
+     * @param tag tag
+     * @param msg message
+     */
+    public static void v(String tag, String msg) {
+        if (LEVEL <= VERBOSE) {
+            Log.v(tag, msg);
+        }
+    }
+
+    /**
+     * Log Debug
+     * @param tag tag
+     * @param msg message
+     */
+    public static void d(String tag, String msg) {
+        if (LEVEL <= DEBUG) {
+            Log.v(tag, msg);
+        }
+    }
+
+    /**
+     * Log Info
+     * @param tag tag
+     * @param msg message
+     */
+    public static void i(String tag, String msg) {
+        if (LEVEL <= INFO) {
+            Log.v(tag, msg);
+        }
+    }
+
+    /**
+     * Log Warn
+     * @param tag tag
+     * @param msg message
+     */
+    public static void w(String tag, String msg) {
+        if (LEVEL <= WARN) {
+            Log.v(tag, msg);
+        }
+    }
+
+    /**
+     * Log Error
+     * @param tag tag
+     * @param msg message
+     */
+    public static void e(String tag, String msg) {
+        if (LEVEL <= ERROR) {
+            Log.v(tag, msg);
+        }
+    }
+}
